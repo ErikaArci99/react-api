b954ea3addf17469635bf83b9e2289ff1fb11e6a
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-13 16:16:14 +0200

    [UPD] chiamata axios eseguita e mostrata in console

--- react-api/components/Main.jsx
+++ react-api/components/Main.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Main = () => {
+    const [actresses, setActresses] = useState([]);
+
+    const fetchActresses = () => {
+        axios.get("https://lanciweb.github.io/demo/api/actresses/")
+            .then((response) => {
+                console.log(response);
+                setActresses(response.data.results);
+            })
+            .catch((error) => {
+                console.error("Errore nella richiesta:", error);
+            });
+    };
+
+    useEffect(() => {
+        fetchActresses();
+    }, []);
+
     return (
-        <div>Main</div>
+        <>
+
+        </>
     )
 }
 
